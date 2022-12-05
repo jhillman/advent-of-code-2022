3e727f87f5af652a8b24684eef059e5b45d7a75e
Jeff Hillman <hillmanapps@gmail.com>
2022-12-05 12:44:01 -0700

    Day 5

---
+++ day05/part1.c
@@ -0,0 +1,9 @@
+/* Day 5, part 1 = PTWLTDSJV */
+
+#include "crates.h"
+
+int main() {
+    moveCrates(V9000);
+
+    return 0;
+}
---
+++ day05/part2.c
@@ -0,0 +1,9 @@
+/* Day 5, part 2 = WZMFVGGZP */
+
+#include "crates.h"
+
+int main() {
+    moveCrates(V9001);
+
+    return 0;
+}
