5ebfce67aa568fa3972db569d13988a01afa1cd1
Jeff Hillman <hillmanapps@gmail.com>
2022-12-06 08:52:14 -0700

    Day 6

---
+++ day06/part1.c
@@ -0,0 +1,10 @@
+/* Day 6, part 1 = 1140 */
+
+#define MARKER_LENGTH 4
+#include "marker.h"
+
+int main() {
+    printf("%d", markerPosition());
+
+    return 0;
+}
---
+++ day06/part2.c
@@ -0,0 +1,10 @@
+/* Day 6, part 2 = 3495 */
+
+#define MARKER_LENGTH 14
+#include "marker.h"
+
+int main() {
+    printf("%d", markerPosition());
+
+    return 0;
+}
