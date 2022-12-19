02b4cb1c5364c5d84dfb21ff5526de2b4d12d4f3
Jeff Hillman <hillmanapps@gmail.com>
2022-12-19 09:50:17 -0700

    Day 18

---
+++ day18/part1.c
@@ -0,0 +1,10 @@
+/* Day 18, part 1 = 3576 */
+
+#include "cubes.h"
+
+int main() {
+    printf("%d", getArea(false));
+
+    return 0;
+}
+
---
+++ day18/part2.c
@@ -0,0 +1,10 @@
+/* Day 18, part 2 = 2066 */
+
+#include "cubes.h"
+
+int main() {
+    printf("%d", getArea(true));
+
+    return 0;
+}
+
