627f0a0a35036eaa0bdc867eba43335bfe2a8ca2
Jeff Hillman <hillmanapps@gmail.com>
2022-12-21 08:29:10 -0700

    Day 20

---
+++ day20/part1.c
@@ -0,0 +1,9 @@
+/* Day 20, part 1 = 2275 */
+
+#include "decrypt.h"
+
+int main() {
+    printf("%ld", decrypt(1, 1));
+
+    return 0;
+}
---
+++ day20/part2.c
@@ -0,0 +1,9 @@
+/* Day 20, part 2 = 4090409331120 */
+
+#include "decrypt.h"
+
+int main() {
+    printf("%ld", decrypt(811589153, 10));
+
+    return 0;
+}
